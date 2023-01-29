5ee8bdc65f622954b83f0898359e2925b3ae4093
EC2 Default User <ec2-user@ip-172-31-2-119.ap-northeast-1.compute.internal>
2023-01-29 14:25:01 +0000

    [finish]3-12

--- db/migrate/20230126093627_removetitle_from_books.rb
+++
@@ -1,4 +0,0 @@
-class RemovetitleFromBooks < ActiveRecord::Migration[6.1]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_26_094147) do
+ActiveRecord::Schema.define(version: 2023_01_29_141356) do
 
   create_table "books", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.string "title"
     t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
